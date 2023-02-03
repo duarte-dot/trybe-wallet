88f0a6213cf3da43e1465df690b4310331a0714f
duarte <gabrieldvr@outlook.com>
2023-02-03 18:21:52 -0300

    requisitos 6 e 7

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,11 +1,60 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 class Table extends Component {
   render() {
+    const { expenses } = this.props;
     return (
-      <div>Table</div>
+      <table>
+        <thead>
+          <tr>
+            <th>Descrição</th>
+            <th>Tag</th>
+            <th>Método de pagamento</th>
+            <th>Valor</th>
+            <th>Moeda</th>
+            <th>Câmbio utilizado</th>
+            <th>Valor convertido</th>
+            <th>Moeda de conversão</th>
+            <th>Editar/Excluir</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            expenses.map((item, index) => (
+              <tr key={ index }>
+                <td>{item.description}</td>
+                <td>{item.tag}</td>
+                <td>{item.method}</td>
+                <td>{parseFloat(item.value).toFixed(2)}</td>
+                <td>{item.exchangeRates[item.currency].name}</td>
+                <td>{parseFloat(item.exchangeRates[item.currency].ask).toFixed(2)}</td>
+                <td>
+                  {parseFloat(
+                    item.value * item.exchangeRates[item.currency].ask,
+                  ).toFixed(2)}
+                </td>
+                <td>REAL</td>
+                <td>
+                  <button>Editar</button>
+                  <button>Excluir</button>
+                </td>
+              </tr>
+            ))
+          }
+        </tbody>
+      </table>
     );
   }
 }
 
-export default Table;
+const mapStateToProps = (state) => ({
+  expenses: state.wallet.expenses,
+});
+
+Table.propTypes = {
+  expenses: PropTypes.instanceOf(Array).isRequired,
+};
+
+export default connect(mapStateToProps)(Table);
--- src/components/Wallet.js
+++ src/components/Wallet.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Header from './Header';
+import Table from './Table';
 import WalletForm from './WalletForm';
 
 class Wallet extends Component {
@@ -8,6 +9,7 @@ class Wallet extends Component {
       <div>
         <Header />
         <WalletForm />
+        <Table />
       </div>
     );
   }
